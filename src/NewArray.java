44b09ebda962fa28fe894b8ce1335c23e6973d5b
Jone23 <jone23@rambler.ru>
2022-09-02 07:25:05 +0300

    add three tipe for each

--- src/NewArray.java
+++ src/NewArray.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class NewArray {
+public class NewArray {
+    public static void main(String[] args) {
+        int [] nums = new int[100];
+
+        for (int i = 0;i < nums.length; i++){
+            nums[i] = i * 10;
+
+           // System.out.println(nums[i]);
+        }
+
+        for (int i: nums) {
+            System.out.println(i);
+        }
+
+
+    }
 }
